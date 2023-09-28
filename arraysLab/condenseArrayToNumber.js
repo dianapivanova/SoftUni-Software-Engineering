0f0e45d20733be70ef9e6523bbc5a2a83dbf93cd
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-09-28 23:37:18 +0300

    Update condenseArrayToNumber.js

--- arraysLab/condenseArrayToNumber.js
+++ arraysLab/condenseArrayToNumber.js
@@ -1,8 +1,16 @@
 function condenseArrayToNumber(arr) {
     let num = [];
+
+    // transforming the array strings into numbers
     for (let i = 0; i < arr.length; i++) {
         num[i] = Number(arr[i]);
     }
+
+    // adding the numbers to the condense string
+    // with every cycle, numbers are being summed
+    // adding the condense result to the num variable
+    // the cycle is making one less rotation because we're already taking the last number in the condense variable
+
     while (num.length > 1) {
         let condense = [];
         for (let i = 0; i < num.length - 1; i++) {
