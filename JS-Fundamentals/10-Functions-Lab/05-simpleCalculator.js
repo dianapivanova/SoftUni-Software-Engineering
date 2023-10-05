f86508245afc1ba383eb05b874b9c28486e37ebb
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-10-05 17:44:18 +0300

    solutions 5-6 functions lab

---
+++ JS-Fundamentals/10-Functions-Lab/05-simpleCalculator.js
@@ -0,0 +1,18 @@
+function simpleCalculator(numOne, numTwo, operator) {
+
+    let multiply = (a, b) => a * b
+    let divide = (a, b) => a / b
+    let add = (a, b) => a + b
+    let subtract = (a, b) => a - b
+    let res = 0
+
+    switch (operator) {
+        case 'multiply': res = multiply(numOne, numTwo); break;
+        case 'divide': res = divide(numOne, numTwo); break;
+        case 'add': res = add(numOne, numTwo); break;
+        case 'subtract': res = subtract(numOne, numTwo); break;
+    }
+
+    console.log(res)
+}
+simpleCalculator(5, 5, 'subtract')
