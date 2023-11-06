33aaea2774a21f040c71264b552d5b1405c4b9e1
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-11-07 00:34:08 +0200

    associative arrays more exercises 1

--- JS-Fundamentals/19-Associative-Arrays-Exercise/09-arenaTier.js
+++ JS-Fundamentals/19-Associative-Arrays-Exercise/09-arenaTier.js
@@ -67,10 +67,15 @@ function arenaTier(arr) {
     }
 }
 arenaTier([
-    'Peter -> BattleCry -> 400',
-    'Alex -> PowerPunch -> 300',
-    'Stefan -> Duck -> 200',
-    'Stefan -> Tiger -> 250',
+    'Peter -> Duck -> 400',
+    'Julius -> Shield -> 150',
+    'Gladius -> Heal -> 200',
+    'Gladius -> Support -> 250',
+    'Gladius -> Shield -> 250',
+    'Peter vs Gladius',
+    'Gladius vs Julius',
+    'Gladius vs Maximilian',
     'Ave Cesar'
 ]
+
 )
