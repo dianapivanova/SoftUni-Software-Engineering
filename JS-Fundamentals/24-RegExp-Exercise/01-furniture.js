d38853b4838ff8c4e2bbda4939dcbec3784df719
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-11-30 21:04:20 +0200

    Update 01-furniture.js

--- JS-Fundamentals/24-RegExp-Exercise/01-furniture.js
+++ JS-Fundamentals/24-RegExp-Exercise/01-furniture.js
@@ -1,43 +1,41 @@
 function furniture(array) {
-    let items = []
-    let totalPrice = 0
-    let pattern = />>(?<name>[A-Z][A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<quantity>\d+)/
+    let pattern = />>(?<furniture>[A-Za-z]+)<<(?<price>\d+\.?\d*)!(?<qty>\d+)/
+    let furnitures = []
+    let totalSum = 0
 
-    let command = array.shift()
+    for (let el of array) {
 
-    while (command !== 'Purchase') {
-        let match = command.match(pattern)
+        let match = el.match(pattern)
 
         if (match) {
-            let { name, price, quantity } = match.groups
+            let { furniture, price, qty } = match.groups
+            price = Number(price)
+            qty = Number(qty)
 
-            let furniturePrice = price * quantity
-
-            items.push(name)
-            totalPrice += furniturePrice
+            furnitures.push(furniture)
+            totalSum += qty * price
         }
 
-        command = array.shift()
+
     }
 
-    console.log('Bought furniture:')
-
-    if (items.length > 0) {
-        items.forEach(x => console.log(x))
+    console.log(`Bought furniture:`)
+    if (furnitures.length > 0) {
+        console.log(`${furnitures.join('\n')}`)
     }
+    console.log(`Total money spend: ${totalSum.toFixed(2)}`)
 
-    console.log(`Total money spend: ${totalPrice.toFixed(2)}`)
 }
-furniture(['>>Sofa<<312.23!3',
-    '>>TV<<300!5',
-    '>Invalid<<!5',
-    'Purchase'])
+// furniture(['>>Sofa<<312.23!3',
+//     '>>TV<<300!5',
+//     '>Invalid<<!5',
+//     'Purchase'])
 
-// furniture(['>>Laptop<<312.2323!3',
-// '>>TV<<300.21314!5',
-// '>Invalid<<!5',
-// '>>TV<<300.21314!20',
-// '>>Invalid<!5',
-// '>>TV<<30.21314!5',
-// '>>Invalid<<!!5',
-// 'Purchase'])
+furniture(['>>Laptop<<312.2323!3',
+    '>>TV<<300.21314!5',
+    '>Invalid<<!5',
+    '>>TV<<300.21314!20',
+    '>>Invalid<!5',
+    '>>TV<<30.21314!5',
+    '>>Invalid<<!!5',
+    'Purchase'])
