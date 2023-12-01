f41b61bb2a379046d1fc22afdb722e920b8b1647
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-12-01 18:22:31 +0200

    text processing solution 01-password adjustment

--- JS-Fundamentals-FinalExam/01-passwordReset.js
+++ JS-Fundamentals-FinalExam/01-passwordReset.js
@@ -1,43 +1,53 @@
 function passwordReset(array) {
     let pass = array.shift()
+
     let command = array.shift()
 
     while (command !== 'Done') {
-        let tokens = command.split(' ')
-        let action = tokens[0]
 
-        if (action == 'TakeOdd') {
+        if (command.includes('TakeOdd')) {
             let rawPass = ''
+
             for (let i = 1; i < pass.length; i += 2) {
                 rawPass += pass[i]
             }
+
             pass = rawPass
             console.log(pass)
-        } else if (action == 'Cut') {
-            let idx = Number(tokens[1])
+
+        } else if (command.includes('Cut')) {
+            let tokens = command.split(' ')
+            let index = Number(tokens[1])
             let length = Number(tokens[2])
-            let removedPart = pass.slice(idx, idx + length)
-            pass = pass.replace(removedPart, '')
+
+            let firstPart = pass.slice(0, index)
+            let secondPart = pass.slice(index + length)
+
+            pass = firstPart + secondPart
             console.log(pass)
-        } else if (action == 'Substitute') {
-            let substr = tokens[1]
+
+        } else if (command.includes('Substitute')) {
+            let tokens = command.split(' ')
+            let substring = tokens[1]
             let substitute = tokens[2]
 
-            if (pass.includes(substr)) {
-                pass = pass.split(substr).join(substitute)
+            if (pass.includes(substring)) {
+                pass = pass.split(substring).join(substitute)
                 console.log(pass)
             } else {
-                console.log('Nothing to replace!')
+                console.log(`Nothing to replace!`)
             }
         }
 
         command = array.shift()
     }
+
     console.log(`Your password is: ${pass}`)
 }
-passwordReset(["up8rgoyg3r1atmlmpiunagt!-irs7!1fgulnnnqy",
+passwordReset(["Siiceercaroetavm!:?:ahsott.:i:nstupmomceqr",
     "TakeOdd",
-    "Cut 18 2",
-    "Substitute ! ***",
-    "Substitute ? .!.",
+    "Cut 15 3",
+    "Substitute :: -",
+    "Substitute | ^",
     "Done"])
+
