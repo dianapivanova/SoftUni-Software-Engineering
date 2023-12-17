467c460f2e2c035c128287037acc8e67d2493047
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-12-17 21:45:03 +0200

    Update 03-sumPrimeNonPrime.js

--- JS-Programming-Basics/12-Nested-Loops-Exercise/03-sumPrimeNonPrime.js
+++ JS-Programming-Basics/12-Nested-Loops-Exercise/03-sumPrimeNonPrime.js
@@ -1,49 +1,34 @@
-function sumPrimeNonPrime(input) {
+function sumPrimeNonPrime(array) {
 
-    let index = 0
-    let sumPrimeNumber = 0
-    let sumNonPrimeNumber = 0
+    let currentRow = array.shift()
 
-    let command = input[index]
-    index++
+    let primeNumSum = 0
+    let nonPrimeNumSum = 0
 
-    while (command !== "stop") {
+    let primeNums = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
 
-        let num = Number(command);
-
+    while (currentRow !== "stop") {
+        let num = Number(currentRow)
         if (num < 0) {
-            console.log("Number is negative.");
-            command = input[index];
-            index++;
-            continue;
+            console.log('Number is negative'); currentRow = array.shift(); continue;
         }
 
-        if (num == 1) {
-            sumPrimeNumber += num;
-            command = input[index];
-            index++;
-            continue;
-        }
-        let isPrime = true;
-
-        for (let i = 2; i < num; i++) {
-            if (num % i === 0) {
-                isPrime = false;
-                break;
-            }
-        }
-
-        if (isPrime) {
-            sumPrimeNumber += num
+        if (primeNums.includes(num)) {
+            primeNumSum += num
         } else {
-            sumNonPrimeNumber += num;
+            nonPrimeNumSum += num
         }
 
-        command = input[index];
-        index++
+        currentRow = array.shift()
     }
 
-    console.log(`Sum of all prime numbers is: ${sumPrimeNumber}`)
-    console.log(`Sum of all non prime numbers is: ${sumNonPrimeNumber}`)
+    console.log(`Sum of all prime numbers is: ${primeNumSum}`)
+    console.log(`Sum of all non prime numbers is: ${nonPrimeNumSum}`)
 }
-sumPrimeNonPrime()
+sumPrimeNonPrime(["3",
+    "9",
+    "0",
+    "7",
+    "19",
+    "4",
+    "stop"])
