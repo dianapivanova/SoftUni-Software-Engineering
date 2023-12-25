b5d1c38175541a0673d916ec768c34cbddc7d5ee
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2023-12-25 18:55:32 +0200

    Update 09-login.js

--- JS-Fundamentals/02-Syntax-Conditional-Statements-Loops-Exercise/09-login.js
+++ JS-Fundamentals/02-Syntax-Conditional-Statements-Loops-Exercise/09-login.js
@@ -1,25 +1,20 @@
-function login(input) {
-    let username = input[0]
-    let pass = ''
+function login(array) {
+    let pass = array.shift()
+    let numAttempts = 0
 
-    for (let i = username.length - 1; i >= 0; i--) {
-        pass += username[i]
-    }
+    for (let attempt of array) {
+        numAttempts++
+        let reversedPass = attempt.split('').reverse().join('')
+        if (reversedPass == pass) {
+            console.log(`User ${pass} logged in.`)
+        } else {
+            if (numAttempts == 4) {
+                console.log(`User ${pass} blocked!`); break;
+            }
+            console.log(`Incorrect password. Try again.`)
 
-    let guess = input[1]
-    let index = 2
-    let counter = 0
-
-    while (guess !== pass) {
-        counter++
-        if (counter >= 4) {
-            console.log(`User ${username} blocked!`); return;
         }
-        console.log('Incorrect password. Try again.')
-        guess = input[index]
-        index++
-    }
 
-    console.log(`User ${username} logged in.`)
+    }
 }
-login()
+login(['sunny', 'rainy', 'cloudy', 'sunny', 'not sunny'])
