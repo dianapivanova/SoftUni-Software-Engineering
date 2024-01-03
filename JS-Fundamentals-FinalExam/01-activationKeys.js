95b5faaf862c1460a8707c11dcb0cc022217a073
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-03 20:02:23 +0200

    Update 01-activationKeys.js

--- JS-Fundamentals-FinalExam/01-activationKeys.js
+++ JS-Fundamentals-FinalExam/01-activationKeys.js
@@ -1,5 +1,5 @@
 function activationKeys(array) {
-    let password = array.shift()
+    let key = array.shift()
 
     let command = array.shift()
 
@@ -7,57 +7,55 @@ function activationKeys(array) {
         let tokens = command.split('>>>')
         let action = tokens[0]
 
-        if (action == 'Contains') {
-            let substring = tokens[1]
-            if (password.includes(substring)) {
-                console.log(`${password} contains ${substring}`)
+        if (action == 'Slice') {
+            let startIdx = Number(tokens[1])
+            let endidx = Number(tokens[2])
+            let firstPart = key.slice(0, startIdx)
+            let secondPart = key.slice(endidx, key.length)
+            key = firstPart + secondPart
+            console.log(key)
+        } else if (action == 'Flip') {
+            let startIdx = Number(tokens[2])
+            let endidx = Number(tokens[3])
+
+            if (tokens[1] == "Upper") {
+                let substr = key.slice(startIdx, endidx)
+                key = key.replace(substr, substr.toUpperCase())
+            } else {
+                let substr = key.slice(startIdx, endidx)
+                key = key.replace(substr, substr.toLowerCase())
+            }
+            console.log(key)
+
+        } else if (action == 'Contains') {
+            let substr = tokens[1]
+            if (key.includes(substr)) {
+                console.log(`${key} contains ${substr}`)
             } else {
                 console.log('Substring not found!')
             }
-        } else if (action == 'Slice') {
-            let startIdx = Number(tokens[1])
-            let endIdx = Number(tokens[2])
-
-            let firstPart = password.slice(0, startIdx)
-            let secondPart = password.slice(endIdx)
-
-            password = firstPart + secondPart
-            console.log(password)
-
-        } else if (action == 'Flip') {
-            let upperLower = tokens[1]
-            let startIdx = Number(tokens[2])
-            let endIdx = Number(tokens[3])
-            let substr = password.slice(startIdx, endIdx)
-
-            if (upperLower == 'Upper') {
-                password = password.replace(substr, substr.toUpperCase())
-            } else {
-                password = password.replace(substr, substr.toLowerCase())
-            }
-
-            console.log(password)
         }
 
         command = array.shift()
     }
 
-    console.log(`Your activation key is: ${password}`)
+
+    console.log(`Your activation key is: ${key}`)
 }
-// activationKeys(["abcdefghijklmnopqrstuvwxyz",
-//     "Slice>>>2>>>6",
-//     "Flip>>>Upper>>>3>>>14",
-//     "Flip>>>Lower>>>5>>>7",
-//     "Contains>>>def",
-//     "Contains>>>deF",
-//     "Generate"])
-
-
-activationKeys(["134softsf5ftuni2020rockz42",
-    "Slice>>>3>>>7",
-    "Contains>>>-rock",
-    "Contains>>>-uni-",
-    "Contains>>>-rocks",
-    "Flip>>>Upper>>>2>>>8",
-    "Flip>>>Lower>>>5>>>11",
+activationKeys(["abcdefghijklmnopqrstuvwxyz",
+    "Slice>>>2>>>6",
+    "Flip>>>Upper>>>3>>>14",
+    "Flip>>>Lower>>>5>>>7",
+    "Contains>>>def",
+    "Contains>>>deF",
     "Generate"])
+
+
+// activationKeys(["134softsf5ftuni2020rockz42",
+//     "Slice>>>3>>>7",
+//     "Contains>>>-rock",
+//     "Contains>>>-uni-",
+//     "Contains>>>-rocks",
+//     "Flip>>>Upper>>>2>>>8",
+//     "Flip>>>Lower>>>5>>>11",
+//     "Generate"])
