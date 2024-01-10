a202e682bb3a6b3e026df704de496f7249dfec9d
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-10 23:03:44 +0200

    Create GCD.js

---
+++ JS-Advanced/syntaxFunctionsStatementsExercise/GCD.js
@@ -0,0 +1,15 @@
+function GCD(num1, num2) {
+
+    num1 = Math.abs(num1);
+    num2 = Math.abs(num2);
+
+    while (num2 !== 0) {
+        const temp = num2;
+        num2 = num1 % num2;
+        num1 = temp;
+    }
+
+
+    console.log(num1)
+}
+GCD(5, 15)
