bc10359a51276e95dfc3a135869bd4394f6329b0
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-10 17:44:03 +0200

    Update stringLength.js

--- JS-Advanced/syntaxFunctionsStatementsLab/stringLength.js
+++ JS-Advanced/syntaxFunctionsStatementsLab/stringLength.js
@@ -1,5 +1,12 @@
 function stringLength(...all) {
 
-    console.log(all)
+    let total = 0
+
+    for (let el of all) {
+        total += el.length
+    }
+
+    console.log(total)
+    console.log(Math.floor(total / all.length))
 }
 stringLength('chocolate', 'ice cream', 'cake')
