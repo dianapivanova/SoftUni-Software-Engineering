51dac1ac166b010b4d3ec42dbfa4cc95a111752b
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-11 21:42:28 +0200

    Update timeToWalk.js

--- JS-Advanced/syntaxFunctionsStatementsExercise/timeToWalk.js
+++ JS-Advanced/syntaxFunctionsStatementsExercise/timeToWalk.js
@@ -1,8 +1,24 @@
 function timeToWalk(steps, footLength, speed) {
-    let speedInM = speed * 1000
-    let speedInS = speedInM * 3600
+    let distance = steps * footLength
+    let speedMInS = speed / 3.6
+    let time = distance / speedMInS
+    let rest = Math.floor(distance / 500)
+    time += rest * 60
 
-    console.log(speedInS)
+
+
+
+    let sec = Math.round(time % 60)
+    let min = Math.floor(time / 60)
+    let hours = Math.floor(min / 60)
+
+    min -= hours * 60
+
+    let hoursToPrint = hours < 10 ? `0${hours}` : `${hours}`;
+    let minToPrint = min < 10 ? `0${min}` : `${min}`;
+    let secToPrint = sec < 10 ? `0${sec}` : `${sec}`;
+
+    console.log(hoursToPrint + ':' + minToPrint + ':' + secToPrint)
 
 }
 timeToWalk(4000, 0.60, 5)
