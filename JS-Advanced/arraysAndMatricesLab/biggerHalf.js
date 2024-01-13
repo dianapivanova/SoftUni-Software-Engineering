4f9263fb74fcad2dbea88cedfd10784b965fcadd
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-13 18:27:12 +0200

    Create biggerHalf.js

---
+++ JS-Advanced/arraysAndMatricesLab/biggerHalf.js
@@ -0,0 +1,7 @@
+function biggerHalf(arr) {
+    let res = arr.sort((a, b) => (a - b))
+    res = res.slice(Math.floor(res.length / 2))
+
+    console.log(res)
+}
+biggerHalf([3, 19, 14, 7, 2, 19, 6])
