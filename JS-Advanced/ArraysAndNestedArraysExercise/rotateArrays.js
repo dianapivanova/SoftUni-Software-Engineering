efe3f04b5c971b8209b286fac89d5e0d45de3609
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-15 16:15:10 +0200

    Create rotateArrays.js

---
+++ JS-Advanced/ArraysAndNestedArraysExercise/rotateArrays.js
@@ -0,0 +1,14 @@
+function rotateArrays(array, rotations) {
+
+    for (let i = 0; i < rotations; i++) {
+        array.unshift(array.pop())
+    }
+
+    console.log(array.join(' '))
+}
+rotateArrays(['Banana',
+    'Orange',
+    'Coconut',
+    'Apple'],
+    15
+)
