f822eaac3d23053506a592a365d15c286020aca5
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-19 14:33:13 +0200

    Create heroes.js

---
+++ JS-Advanced/objectsAndCompositionExercise/heroes.js
@@ -0,0 +1,55 @@
+function solve() {
+
+    function fighter(name) {
+        let state = {
+            name,
+            health: 100,
+            stamina: 100
+        }
+
+        return Object.assign(state, canFight(state))
+    }
+
+    function mage(name) {
+        let state = {
+            name,
+            health: 100,
+            mana: 100
+        }
+
+        return Object.assign(state, canCast(state))
+    }
+
+    function canCast(state) {
+        return {
+            cast: function (spell) {
+                console.log(state.name + ' cast ' + spell);
+                state.mana--;
+            }
+        }
+    }
+
+    function canFight(state) {
+        return {
+            fight: function () {
+                console.log(state.name + ' slashes at the foe!');
+                state.stamina--;
+            }
+        }
+    }
+
+    return { fighter, mage }
+
+}
+
+let create = solve();
+const scorcher = create.mage("Scorcher");
+scorcher.cast("fireball");
+scorcher.cast("thunder");
+scorcher.cast("light");
+
+const scorcher2 = create.fighter("Scorcher 2");
+scorcher2.fight();
+
+console.log(scorcher2.stamina);
+console.log(scorcher.mana);
