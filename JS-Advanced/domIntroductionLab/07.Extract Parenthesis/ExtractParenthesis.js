f833549fd41d15cc05f824a5ee00b6855d711300
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-21 14:43:39 +0200

    Update ExtractParenthesis.js

--- JS-Advanced/domIntroductionLab/07.Extract Parenthesis/ExtractParenthesis.js
+++ JS-Advanced/domIntroductionLab/07.Extract Parenthesis/ExtractParenthesis.js
@@ -1,3 +1,15 @@
 function extract(content) {
 
+    let para = document.getElementById(content).textContent
+    let pattern = /\((?<captured>([^)]+))\)/g
+    let result = []
+
+    let match = para.matchAll(pattern)
+
+    for (let row of match) {
+
+        result.push(row[1])
+    }
+
+    alert(result.join('; '));
 }
