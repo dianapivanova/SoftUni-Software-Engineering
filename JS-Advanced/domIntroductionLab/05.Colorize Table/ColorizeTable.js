0da75cb2f7115f492815fafb12a7294a85c431c9
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-21 14:21:11 +0200

    Update ColorizeTable.js

--- JS-Advanced/domIntroductionLab/05.Colorize Table/ColorizeTable.js
+++ JS-Advanced/domIntroductionLab/05.Colorize Table/ColorizeTable.js
@@ -1,6 +1,13 @@
+// function colorize() {
+//     let rows = Array.from(document.getElementsByTagName('tr'))
+//     for (let i = 1; i < rows.length; i += 2) {
+//         rows[i].style.background = 'teal'
+//     }
+// }
+
 function colorize() {
-    let rows = Array.from(document.getElementsByTagName('tr'))
-    for (let i = 1; i < rows.length; i += 2) {
-        rows[i].style.background = 'teal'
+    const rows = Array.from(document.querySelectorAll('tr:nth-child(even)'))
+    for (let row of rows) {
+        row.style.background = 'teal'
     }
 }
