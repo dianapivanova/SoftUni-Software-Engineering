cdc60f4848aca6306ebebfe263fad4b7e2793964
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-21 13:58:37 +0200

    Update CollectListItems.js

--- JS-Advanced/domIntroductionLab/02.Collect List Items/CollectListItems.js
+++ JS-Advanced/domIntroductionLab/02.Collect List Items/CollectListItems.js
@@ -1,3 +1,12 @@
 function extractText() {
-    // TODO
+    let content = document.getElementById('items')
+    let result = []
+
+    let array = Array.from(content.children)
+    for (let el of array) {
+        result.push(el.textContent)
+    }
+
+    const textArea = document.getElementById('result')
+    textArea.value = result.join('\n')
 }
