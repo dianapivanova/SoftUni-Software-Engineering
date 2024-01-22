9ed9980e67eb3b7a9a55045ac07f94c1a04b0aa4
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-23 00:03:07 +0200

    create subtract.js

---
+++ JS-Advanced/domIntroductionExercise/01. Subtraction/subtract.js
@@ -0,0 +1,8 @@
+function subtract() {
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+    let divide = Number(firstNum) - Number(secondNum)
+    let result = document.getElementById('result')
+    result.textContent = divide
+    console.log(result)
+}
