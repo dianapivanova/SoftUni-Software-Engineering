6f469d9d8a0ea10fa439f7dbe00871d09fe364db
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-23 15:39:26 +0200

    Update search.js

--- JS-Advanced/domIntroductionExercise/04. Search in List/search.js
+++ JS-Advanced/domIntroductionExercise/04. Search in List/search.js
@@ -1,3 +1,24 @@
 function search() {
-   // TODO
+   let tableRef = document.querySelectorAll('ul li')
+   let matches = 0
+   let divRes = document.getElementById('result')
+
+   let searchedWord = document.getElementById('searchText')
+
+   for (let row of tableRef) {
+      row.style.fontWeight = 'normal'
+      row.style.textDecoration = 'none'
+   }
+
+   for (let row of tableRef) {
+      if (row.textContent.includes(searchedWord.value)) {
+         matches++
+         row.style.fontWeight = 'bold'
+         row.style.textDecoration = 'underline'
+
+      }
+   }
+   searchedWord.value = ''
+
+   divRes.textContent = `${matches} matches found`
 }
