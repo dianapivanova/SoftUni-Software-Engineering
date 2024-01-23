cf2863dbea49c58a14a49f13320be39e0d1b6722
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-23 11:31:36 +0200

    Update solution.js

--- JS-Advanced/domIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/domIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO...
+  let textReference = document.getElementById('text')
+  let conventionRef = document.getElementById('naming-convention')
+  let result = ''
+
+  let textArr = textReference.value.toLowerCase().split(' ')
+
+  for (let word of textArr) {
+    if (conventionRef.value == 'Camel Case') {
+      if (result == '') {
+        result += word
+      } else {
+        result += word[0].toUpperCase() + word.slice(1)
+      }
+    } else if (conventionRef.value == 'Pascal Case') {
+      result += word[0].toUpperCase() + word.slice(1)
+    } else {
+      result = 'Error!'
+    }
+  }
+
+  let resultArea = document.getElementById('result')
+  resultArea.textContent = result
+
 }
