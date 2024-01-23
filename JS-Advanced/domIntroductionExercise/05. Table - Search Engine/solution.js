c2d701cf2d2dbb962d3d8600fb93f43c6656ff0d
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-23 16:16:45 +0200

    Update solution.js

--- JS-Advanced/domIntroductionExercise/05. Table - Search Engine/solution.js
+++ JS-Advanced/domIntroductionExercise/05. Table - Search Engine/solution.js
@@ -1,8 +1,25 @@
 function solve() {
    document.querySelector('#searchBtn').addEventListener('click', onClick);
+   let tableRows = Array.from(document.querySelectorAll('tbody tr'))
+   let searchRef = document.getElementById('searchField')
 
    function onClick() {
-      //   TODO:
 
+      let searchedWord = searchRef.value
+
+      for (let tableRow of tableRows) {
+         let tableCols = Array.from(tableRow.querySelectorAll('td'))
+         for (let col of tableCols) {
+            if (col.textContent.includes(searchedWord)) {
+               tableRow.classList.add('select'); break;
+            } else {
+               tableRow.classList.remove('select');
+            }
+         }
+      }
+
+      searchRef.value = "";
    }
+
+
 }
