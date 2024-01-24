1bb461fad3b1185964bb8775ede9bd5e29269f23
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-24 20:44:28 +0200

    Update SumTable.js

--- JS-Advanced/domIntroductionLab/06.Sum Table/SumTable.js
+++ JS-Advanced/domIntroductionLab/06.Sum Table/SumTable.js
@@ -1,12 +1,12 @@
 function sumTable() {
-    let table = document.querySelectorAll('table tr');
+    let tableRows = document.getElementsByTagName('tr')
+    let output = document.getElementById('sum')
     let total = 0
 
-    for (let i = 1; i < table.length; i++) {
-        let cols = table[i].children
-        let sum = cols[cols.length - 1].textContent
-        total += Number(sum)
+    for (let i = 1; i < tableRows.length - 1; i++) {
+        let num = tableRows[i].getElementsByTagName('td')[1]
+        total += Number(num.textContent)
     }
 
-    document.getElementById('sum').textContent = total
+    output.textContent = total
 }
