b77b48b4273aacb7ec22c4de4714e86fd7d048c6
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-24 19:35:52 +0200

    Update app.js

--- JS-Advanced/domManipulationsAndEventsLab/02. Delete-From-Table/app.js
+++ JS-Advanced/domManipulationsAndEventsLab/02. Delete-From-Table/app.js
@@ -1,3 +1,24 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    let emailInput = document.querySelector('[name = "email"]').value;
+    let tableRows = document.querySelectorAll('#customers tbody tr')
+    let tableRef = document.getElementsByTagName('tbody')
+    let resultRef = document.getElementById('result')
+
+    let isRemoved = false;
+
+    for (let row of tableRows) {
+        let email = row.children[1].textContent
+        if (email == emailInput) {
+            row.remove()
+            isRemoved = true;
+        }
+
+    }
+
+    if (isRemoved) {
+        resultRef.textContent = 'Deleted'
+    } else {
+        resultRef.textContent = 'Not found.'
+    }
+
 }
