54ebb1ac3a00ff8a02295801c5733e45cea430c1
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-25 15:44:33 +0200

    Update solution.js

--- JS-Advanced/domIntroductionExercise/05. Table - Search Engine/solution.js
+++ JS-Advanced/domIntroductionExercise/05. Table - Search Engine/solution.js
@@ -1,24 +1,27 @@
 function solve() {
+
    document.querySelector('#searchBtn').addEventListener('click', onClick);
-   let tableRows = Array.from(document.querySelectorAll('tbody tr'))
-   let searchRef = document.getElementById('searchField')
 
    function onClick() {
+      let tableArr = Array.from(document.querySelectorAll('tbody tr'))
+      let searchedWord = document.getElementById("searchField")
 
-      let searchedWord = searchRef.value
 
-      for (let tableRow of tableRows) {
-         let tableCols = Array.from(tableRow.querySelectorAll('td'))
-         for (let col of tableCols) {
-            if (col.textContent.includes(searchedWord)) {
-               tableRow.classList.add('select'); break;
-            } else {
-               tableRow.classList.remove('select');
+      for (let row of tableArr) {
+         let isSelected = false;
+         let tableData = row.getElementsByTagName('td')
+
+         for (let data of tableData) {
+            if (data.textContent.includes(searchedWord.value)) {
+               row.classList.add('select'); isSelected = true; break;
             }
          }
-      }
 
-      searchRef.value = "";
+         if (!isSelected) {
+            row.classList.remove('select')
+         }
+      }
+      searchedWord.value = ''
    }
 
 
