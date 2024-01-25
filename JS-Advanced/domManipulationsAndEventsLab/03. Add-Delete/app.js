8fda5a992bbbf52ee0cd57b4a7f10eaaa88a54a0
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-25 21:48:04 +0200

    Update app.js

--- JS-Advanced/domManipulationsAndEventsLab/03. Add-Delete/app.js
+++ JS-Advanced/domManipulationsAndEventsLab/03. Add-Delete/app.js
@@ -1,3 +1,26 @@
 function addItem() {
-    //TODO...
+    let newText = document.getElementById("newItemText");
+    let appendTo = document.getElementById("items");
+
+    if (newText.value !== "") {
+        let newElement = document.createElement('li');
+        newElement.textContent = newText.value;
+
+        const deleteBtn = document.createElement('a')
+        deleteBtn.textContent = '[Delete]'
+        deleteBtn.href = '#' //a tag with no href will be disabled and the client won't be able to click;
+        //we are using # as a link in order not to use the link as a navigation (not to redirect)
+        newElement.appendChild(deleteBtn);
+        deleteBtn.onclick = onDelete
+        // deleteBtn.addEventListener('click', onDelete)
+
+        appendTo.appendChild(newElement);
+        newText.value = '';
+
+        function onDelete(event) {
+            const deletedElement = event.target.parentElement
+            deletedElement.remove()
+
+        }
+    }
 }
