c2c7ad70ac9d73984d5ce0ee7ff0002441e71141
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-26 15:34:41 +0200

    Update solution.js

--- JS-Advanced/domManipulationsAndEventsLab/07. Shopping-Cart/solution.js
+++ JS-Advanced/domManipulationsAndEventsLab/07. Shopping-Cart/solution.js
@@ -1,27 +1,30 @@
 function solve() {
-   Array.from(document.querySelectorAll('.add-product')).forEach(x => x.addEventListener('click', add));
-   document.querySelector('.checkout').addEventListener('click', checkout);
+   Array.from(document.querySelectorAll("button.add-product")).forEach(x => x.addEventListener('click', onAdd));
+   document.querySelector(".checkout").addEventListener("click", checkout);
    const products = {
-      'Bread': 0.8,
+      'Bread': 0.80,
       'Milk': 1.09,
       'Tomatoes': 0.99
    }
+
+   let output = document.querySelector("textarea");
    const bought = [];
-   const textArea = document.querySelector('textarea');
-   let cost = 0;
+   let total = 0;
    let textOutput = '';
 
-   function add(e) {
-      const current = e.target.parentNode.parentNode.querySelector('.product-title').textContent;
-      textOutput = `Added ${current} for ${products[current].toFixed(2)} to the cart.\n`;
-      cost += products[current];
-      textArea.value += textOutput;
-      if (!bought.includes(current)) { bought.push(current) }
+   function onAdd(event) {
+      const currentProduct = event.target.parentNode.parentNode.querySelector(".product-title").textContent;
+      textOutput = `Added ${currentProduct} for ${products[currentProduct].toFixed(2)} to the cart.\n`;
+      total += products[currentProduct];
+      if (!bought.includes(currentProduct)) {
+         bought.push(currentProduct);
+      }
+      output.value += textOutput
    }
 
    function checkout() {
-      textOutput = `You bought ${bought.join(', ')} for ${cost.toFixed(2)}.`;
-      textArea.value += textOutput;
+      textOutput = `You bought ${bought.join(', ')} for ${total.toFixed(2)}.`;
+      output.value += textOutput;
       Array.from(document.querySelectorAll('button')).forEach(x => x.disabled = true);
    }
 }
