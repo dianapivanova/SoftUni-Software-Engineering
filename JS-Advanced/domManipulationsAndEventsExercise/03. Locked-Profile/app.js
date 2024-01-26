5432ccb5d8b551b2a24b26e907395cc04f42c546
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-26 20:03:27 +0200

    Update app.js

--- JS-Advanced/domManipulationsAndEventsExercise/03. Locked-Profile/app.js
+++ JS-Advanced/domManipulationsAndEventsExercise/03. Locked-Profile/app.js
@@ -1,3 +1,35 @@
 function lockedProfile() {
-    console.log('TODO...')
+    //constants 
+    const SHOW_MORE = "Show more";
+    const HIDE_IT = "Hide it";
+
+    //get button elements
+    const btnElements = Array.from(document.querySelectorAll('div button'));
+
+    // attach event listeners
+    for (let btn of btnElements) {
+        btn.addEventListener("click", show)
+    }
+
+    // on click functionality
+    function show(event) {
+        const divChildren = Array.from(event.target.parentElement.children);
+        const isLocked = divChildren[2].checked;
+        if (isLocked) {
+            return;
+        }
+
+        const hiddenFieldElements = event.target.previousElementSibling;
+        if (event.target.textContent == SHOW_MORE) {
+            hiddenFieldElements.style.display = "inline"
+            event.target.textContent = HIDE_IT;
+
+            return;
+        }
+
+        hiddenFieldElements.style.display = "none";
+        event.target.textContent = SHOW_MORE;
+    }
+
+
 }
