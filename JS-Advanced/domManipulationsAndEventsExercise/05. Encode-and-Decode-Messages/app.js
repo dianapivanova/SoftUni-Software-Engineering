00a6f2b009a9314ee777f38994c14712d91525db
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-26 22:51:07 +0200

    Update app.js

--- JS-Advanced/domManipulationsAndEventsExercise/05. Encode-and-Decode-Messages/app.js
+++ JS-Advanced/domManipulationsAndEventsExercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,35 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const receivedMessage = document.querySelectorAll('textarea')[0];
+    const sendMessage = document.querySelectorAll('textarea')[1];
+    const encodeBtn = document.querySelectorAll('button')[0];
+    const decodeBtn = document.querySelectorAll('button')[1];
+
+    encodeBtn.addEventListener('click', onEncode);
+    decodeBtn.addEventListener('click', onDecode);
+
+    function onEncode() {
+        let textToEncrypt = receivedMessage.value.split('')
+        let encryptedResult = ''
+
+        for (let char of textToEncrypt) {
+            let newChar = String.fromCharCode(char.charCodeAt(0) + 1)
+            encryptedResult += newChar
+        }
+
+        receivedMessage.value = '';
+        sendMessage.value = encryptedResult;
+    }
+
+    function onDecode() {
+        let textToDecrypt = sendMessage.value.split('')
+        let decryptedResult = ''
+
+        for (let char of textToDecrypt) {
+            let newChar = String.fromCharCode(char.charCodeAt(0) - 1)
+            decryptedResult += newChar
+        }
+
+        sendMessage.value = decryptedResult;
+    }
+
 }
