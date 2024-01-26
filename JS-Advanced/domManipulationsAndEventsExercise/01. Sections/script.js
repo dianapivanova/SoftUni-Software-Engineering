86c4e4a5f92a5e73f2ee6e511950c516074365cc
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-26 17:04:44 +0200

    Update script.js

--- JS-Advanced/domManipulationsAndEventsExercise/01. Sections/script.js
+++ JS-Advanced/domManipulationsAndEventsExercise/01. Sections/script.js
@@ -1,3 +1,20 @@
 function create(words) {
-   console.log('TODO:...');
+
+
+   for (let word of words) {
+
+      let divEl = document.createElement('div');
+      let pEl = document.createElement('p');
+      pEl.style.display = 'none';
+      pEl.textContent = word;
+      divEl.appendChild(pEl);
+      let appendToEl = document.getElementById('content');
+      appendToEl.appendChild(divEl);
+      divEl.addEventListener("click", onClick);
+   }
+
+   function onClick(e) {
+
+      e.target.children[0].style.display = "block"
+   }
 }
