c4566d0920ebfab6e268e7cf556ea592323b47f2
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-26 19:21:30 +0200

    Update solution.js

--- JS-Advanced/domManipulationsAndEventsExercise/02. Time-Converter/solution.js
+++ JS-Advanced/domManipulationsAndEventsExercise/02. Time-Converter/solution.js
@@ -1,48 +1,46 @@
 function attachEventsListeners() {
 
     //confir
-    const RATIONS = {
+    const RATIO = {
         days: 1,
         hours: 24,
         minutes: 1440,
         seconds: 86400
-    };
+    }
 
     //helper functions
     function convert(value, unit) {
-        const inDays = value / RATIONS[unit];
+        let inDays = value / RATIO[unit]
 
         return {
             days: inDays,
-            hours: inDays * RATIONS.hours,
-            minutes: inDays * RATIONS.minutes,
-            seconds: inDays * RATIONS.seconds
+            hours: inDays * RATIO.hours,
+            minutes: inDays * RATIO.minutes,
+            seconds: inDays * RATIO.seconds
         }
     }
 
     //capture metrics
-    let daysInput = document.getElementById('days');
-    let hoursInput = document.getElementById('hours');
-    let minutesInput = document.getElementById('minutes');
-    let secondsInput = document.getElementById('seconds');
+    let daysInputValue = document.getElementById('days');
+    let hoursInputValue = document.getElementById('hours');
+    let minutesInputValue = document.getElementById('minutes');
+    let secondsInputValue = document.getElementById('seconds');
+
 
     //event listener setup
-    document.querySelector('main').addEventListener('click', onConvert)
-
+    document.querySelector('main').addEventListener('click', onConvert);
 
     function onConvert(e) {
-        if (e.target.tagName === "INPUT" && e.target.type == "button") {
-            const input = e.target.parentElement.querySelector('input[type="text"]');
-            const inputValue = Number(input.value);
-            const time = convert(inputValue, input.id)
+        if (e.target.tagName == "INPUT" && e.target.type == "button") {
+            let input = e.target.parentElement.querySelector('[type="text"]');
+            let inputValue = Number(input.value);
+            let time = convert(inputValue, input.id)
 
-            daysInput.value = time.days;
-            hoursInput.value = time.hours;
-            minutesInput.value = time.minutes;
-            secondsInput.value = time.seconds;
+            daysInputValue.value = time.days;
+            hoursInputValue.value = time.hours;
+            minutesInputValue.value = time.minutes;
+            secondsInputValue.value = time.seconds;
         }
-
-
     }
 
 }
