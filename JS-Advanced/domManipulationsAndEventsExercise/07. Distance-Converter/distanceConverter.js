68941292c98bdcd1a41a92ff81d17b800b1033fd
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-27 20:34:51 +0200

    Update distanceConverter.js

--- JS-Advanced/domManipulationsAndEventsExercise/07. Distance-Converter/distanceConverter.js
+++ JS-Advanced/domManipulationsAndEventsExercise/07. Distance-Converter/distanceConverter.js
@@ -1,3 +1,35 @@
 function attachEventsListeners() {
-    console.log('TODO:...');
+
+    const CONVERTIONS = {
+        km: 1000,
+        m: 1,
+        cm: 0.01,
+        mm: 0.001,
+        mi: 1609.34,
+        yrd: 0.9144,
+        ft: 0.3048,
+        in: 0.0254
+    }
+
+
+    function convert(receivedValue, receivedDistance, resultDistance) {
+        let inputInMeters = receivedValue * CONVERTIONS[receivedDistance];
+        let result = inputInMeters / CONVERTIONS[resultDistance];
+
+        return result;
+    }
+    let inputArea = document.getElementById("inputDistance");
+    let inputOptions = document.getElementById("inputUnits");
+    let outputArea = document.getElementById("outputDistance");
+    let outputOptions = document.getElementById("outputUnits");
+
+    let btnConvert = document.getElementById("convert");
+    btnConvert.addEventListener('click', onClick);
+
+    function onClick() {
+        let receivedValue = Number(inputArea.value);
+        let receivedDistance = inputOptions.value
+        let resultDistance = outputOptions.value;
+        outputArea.value = convert(receivedValue, receivedDistance, resultDistance)
+    }
 }
