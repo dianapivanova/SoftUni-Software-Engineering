b16cb5961744ac464bd9018f9951876fd1e85109
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-30 12:02:05 +0200

    Create createFormatter.js

---
+++ JS-Advanced/functionsAdvancedLab/createFormatter.js
@@ -0,0 +1,14 @@
+function createFormatter(separator, symbol, symbolFirst, currencyFormatter) {
+    let formatter = function (value) {
+        return currencyFormatter(separator, symbol, symbolFirst, value);
+    }
+    return formatter;
+}
+
+
+
+let dollarFormatter = createFormatter(',', '$', true, currencyFormatter);
+console.log(dollarFormatter(5345));
+console.log(dollarFormatter(3.1429));
+console.log(dollarFormatter(2.709));
+
