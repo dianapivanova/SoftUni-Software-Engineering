16199f2812c4b0455cf80b2afa2f9b9bb609a119
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-31 13:39:33 +0200

    Update app.js

--- JS-Advanced/functionsAdvancedExercise/07. Simple Calculator/app.js
+++ JS-Advanced/functionsAdvancedExercise/07. Simple Calculator/app.js
@@ -1,6 +1,26 @@
 function calculator() {
-    // TODO:
+
+    debugger
+    const html = {
+        n1: '',
+        n2: '',
+        res: ''
+    }
+
+    return {
+        init: (selector1, selector2, resultSelector) => {
+            html.n1 = document.querySelector(selector1);
+            html.n2 = document.querySelector(selector2);
+            html.res = document.querySelector(resultSelector)
+        },
+        add: () => html.res.value = Number(html.n1.value) + Number(html.n2.value),
+        subtract: () => html.res.value = Number(html.n1.value) - Number(html.n2.value)
+    }
 }
+
+const calculate = calculator();
+calculate.init('#num1', '#num2', '#result');
+
 
 
 
