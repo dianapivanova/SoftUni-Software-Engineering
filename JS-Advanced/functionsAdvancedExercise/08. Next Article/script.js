fa941f081c00a7da4dcd70c799c6de4ac6cc8e7d
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-01-31 14:55:45 +0200

    Update script.js

--- JS-Advanced/functionsAdvancedExercise/08. Next Article/script.js
+++ JS-Advanced/functionsAdvancedExercise/08. Next Article/script.js
@@ -1,3 +1,14 @@
 function getArticleGenerator(articles) {
-    // TODO
+    const output = document.getElementById('content')
+    let ourArrCopy = articles.slice();
+
+    return function getNextOne() {
+        if (ourArrCopy[0] !== undefined) {
+            let article = document.createElement('article');
+            article.textContent = articles.shift()
+            output.appendChild(article)
+        }
+
+        return getNextOne;
+    }
 }
