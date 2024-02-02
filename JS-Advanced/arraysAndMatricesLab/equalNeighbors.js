b7b8554c3f6b8055cc78645fc04e66f0c462bcf5
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-02 17:55:20 +0200

    Update equalNeighbors.js

--- JS-Advanced/arraysAndMatricesLab/equalNeighbors.js
+++ JS-Advanced/arraysAndMatricesLab/equalNeighbors.js
@@ -1,25 +1,29 @@
 function equalNeighbors(matrix) {
+    let equalPairs = 0
 
-    let neighbors = 0;
-
-    for (let row = 0; row < matrix.length - 1; row++) {
+    for (let row = 0; row < matrix.length; row++) {
         for (let col = 0; col < matrix[row].length; col++) {
-            if (matrix[row][col] == matrix[row + 1][col]) {
-                neighbors++;
-            }
-            if (matrix[row][col] == matrix[row][col + 1]) {
-                neighbors++;
-            }
-            if (row == matrix.length - 2 && matrix[row + 1][col] == matrix[row + 1][col + 1]) {
-                neighbors++;
+            if (row !== matrix.length - 1) {
+                if (matrix[row][col] == matrix[row + 1][col]) {
+                    equalPairs++;
+                }
+                if (matrix[row][col] == matrix[row][col + 1]) {
+                    equalPairs++;
+                }
+            } else {
+                if (matrix[row][col] == matrix[row][col + 1]
+                ) {
+                    equalPairs++;
+                }
             }
         }
     }
-    console.log(neighbors);
+
+    console.log(equalPairs)
 }
 equalNeighbors([
-    ['2', '3', '4', '7', '0'],
-    ['4', '3', '5', '3', '4'],
-    ['2', '3', '5', '4', '2'],
-    ['9', '8', '7', '5', '4']]
+    ['test', 'yes', 'yo', 'ho'],
+    ['done', 'done', 'yo', '6'],
+    ['not', 'done', 'yet', 'yo']]
+
 )
