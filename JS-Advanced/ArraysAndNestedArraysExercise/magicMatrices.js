bdb0d921d5e32c6c27c0c1d7d1a1ca27e93125eb
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-02 19:06:36 +0200

    Update magicMatrices.js

--- JS-Advanced/ArraysAndNestedArraysExercise/magicMatrices.js
+++ JS-Advanced/ArraysAndNestedArraysExercise/magicMatrices.js
@@ -1,20 +1,33 @@
 function magicMatrices(matrix) {
-    for (let i = 0; i < matrix.length - 1; i++) {
-        let rowOne = matrix[i].reduce((acc, value) => acc + value, 0); // iterates through the rows and sums them
-        let rowTwo = matrix[i + 1].reduce((acc, value) => acc + value, 0); // iterates through the rows and sums them
-        let colOne = matrix.reduce((acc, value) => acc + value[i], 0); // iterates through the whole matrix and sum the el of [i]
-        let colTwo = matrix.reduce((acc, value) => acc + value[i + 1], 0); // iterates through the rows and sums them
 
-        if (rowOne !== rowTwo || colOne !== colTwo) {
-            return false;
+    let rowSum = matrix[0].reduce((acc, value) => (acc + value))
+
+    for (let row = 1; row < matrix.length; row++) {
+        let currentRowSum = 0
+        currentRowSum = matrix[row].reduce((acc, value) => acc + value);
+        if (currentRowSum !== rowSum) {
+            console.log('false'); return;
         }
     }
 
-    return true;
+    for (let col = 0; col < matrix[0].length; col++) {
+        let currentColNum = 0;
+
+        for (let row = 0; row < matrix.length; row++) {
+            currentColNum += matrix[row][col]
+        }
+
+        if (currentColNum !== rowSum) {
+            console.log('false'); return;
+        }
+    }
+
+    console.log('true')
+
 }
 
-magicMatrices([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-])
+magicMatrices([[11, 32, 45],
+[21, 0, 1],
+[21, 1, 1]]
+
+)
