fae18d4a4512938c008f3f77fef44513c9747801
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-02 16:55:20 +0200

    update diagonalSums.js

--- JS-Advanced/arraysAndMatricesLab/diagonalSums.js
+++ JS-Advanced/arraysAndMatricesLab/diagonalSums.js
@@ -2,20 +2,20 @@ function diagonalSums(arr) {
     let diagonal1Sum = 0;
     let diagonal2Sum = 0;
 
-    for (let rowId = 0; rowId < arr.length; rowId++) {
-        for (let colId = 0; colId < arr[rowId].length; colId++) {
-            if (rowId === colId) {
-                diagonal1Sum += arr[rowId][colId];
+    for (let row = 0; row < arr.length; row++) {
+        for (let col = 0; col < arr[row].length; col++) {
+
+            if (row == col) {
+                diagonal1Sum += arr[row][col]
             }
 
-            // The correct calculation for the second diagonal is arr[rowId][arr[rowId].length - 1 - rowId]
-            if (rowId + colId === arr[rowId].length - 1) {
-                diagonal2Sum += arr[rowId][colId];
+            if (col == arr[row].length - row - 1) {
+                diagonal2Sum += arr[row][col]
             }
         }
     }
 
-    return (`${diagonal1Sum} ${diagonal2Sum}`)
+    console.log(diagonal1Sum, diagonal2Sum)
 }
 diagonalSums([[3, 5, 17],
 [-1, 7, 14],
