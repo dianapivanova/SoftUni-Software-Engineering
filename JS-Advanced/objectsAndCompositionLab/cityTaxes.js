62d9057600bde00f7d6c1c3aa1dfd93ba6908cac
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-04 11:14:48 +0200

    Update cityTaxes.js

--- JS-Advanced/objectsAndCompositionLab/cityTaxes.js
+++ JS-Advanced/objectsAndCompositionLab/cityTaxes.js
@@ -1,24 +1,20 @@
 function cityTaxes(name, population, treasury) {
 
-    let city = {
-        name,
-        population,
-        treasury,
+    return {
+        name: name,
+        population: population,
+        treasury: treasury,
         taxRate: 10,
         collectTaxes() {
-            this.treasury += this.population * this.taxRate
+            this.treasury += Math.floor(this.population * this.taxRate)
         },
         applyGrowth(percentage) {
-            this.population += Math.floor(this.population * (percentage / 100))
+            this.population += Math.floor(this.population * percentage / 100)
         },
         applyRecession(percentage) {
-            this.treasury -= Math.floor(this.treasury * (percentage / 100))
+            this.treasury -= Math.floor(this.treasury * percentage / 100)
         }
-
     }
-
-    return city
-
 }
 
 const city =
