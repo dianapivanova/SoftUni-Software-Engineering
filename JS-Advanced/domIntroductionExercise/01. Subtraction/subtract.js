4207e18988a57b9e145a2fc26ec64dbfc768826d
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-05 19:02:49 +0200

    Update subtract.js

--- JS-Advanced/domIntroductionExercise/01. Subtraction/subtract.js
+++ JS-Advanced/domIntroductionExercise/01. Subtraction/subtract.js
@@ -1,6 +1,6 @@
 function subtract() {
-    let firstNum = document.getElementById("firstNumber").value;
-    let secondNum = document.getElementById("secondNumber").value;
-    let resultArea = document.getElementById("result")
-    resultArea.textContent = Number(firstNum) - Number(secondNum);
+    let firstNum = document.getElementById('firstNumber').value;
+    let secondNum = document.getElementById('secondNumber').value;
+    let resultArea = document.getElementById('result')
+    resultArea.textContent = Number(firstNum) - Number(secondNum)
 }
