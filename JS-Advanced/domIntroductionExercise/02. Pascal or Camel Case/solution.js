a41218afc8950caea58cd0ade24b4a52207d9b87
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-05 19:13:18 +0200

    Update solution.js

--- JS-Advanced/domIntroductionExercise/02. Pascal or Camel Case/solution.js
+++ JS-Advanced/domIntroductionExercise/02. Pascal or Camel Case/solution.js
@@ -2,33 +2,28 @@ function solve() {
   const CAMEL_CASE = "Camel Case"
   const PASCAL_CASE = "Pascal Case"
 
-  let textAreaValue = document.getElementById("text").value;
-  let conventionAreaValue = document.getElementById("naming-convention").value;
-  let resultArea = document.getElementById("result");
+  const textRef = document.getElementById('text');
+  let textToModify = textRef.value.split(' ')
+  const namingConvention = document.getElementById('naming-convention')
+  const resultArea = document.getElementById('result')
 
-  let textToAdjust = textAreaValue.toLowerCase().split(' ')
+  let result = ''
 
-  let result = ""
+  for (let word of textToModify) {
 
-  if (conventionAreaValue !== CAMEL_CASE && conventionAreaValue !== PASCAL_CASE) {
-    resultArea.textContent = "Error!"; return;
-  }
-
-  if (conventionAreaValue == CAMEL_CASE) {
-    for (let word of textToAdjust) {
-      if (result == "") {
-        result += word
+    if (namingConvention.value == CAMEL_CASE) {
+      if (result == '') {
+        result += word.toLowerCase()
       } else {
-        result += word[0].toUpperCase() + word.slice(1)
+        result += word[0].toUpperCase() + word.slice(1).toLowerCase()
       }
-    }
-
-  } else if (conventionAreaValue == PASCAL_CASE) {
-    for (let word of textToAdjust) {
-      result += word[0].toUpperCase() + word.slice(1)
+    } else if (namingConvention.value == PASCAL_CASE) {
+      result += word[0].toUpperCase() + word.slice(1).toLowerCase()
+    } else {
+      result = 'Error'; break;
     }
   }
 
-  resultArea.textContent = result;
+  resultArea.textContent = result
 
 }
