ecf7c6d4ba91c3da5b083f894c53bfacbf22b1ae
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-06 16:49:57 +0200

    Update app.js

--- JS-Advanced/domManipulationsAndEventsLab/01. List-Of-Items/app.js
+++ JS-Advanced/domManipulationsAndEventsLab/01. List-Of-Items/app.js
@@ -1,11 +1,12 @@
 function addItem() {
-    let newText = document.getElementById("newItemText");
-    let appendTo = document.getElementById("items");
+    let outputField = document.getElementById('newItemText');
+    let ulRef = document.getElementById('items')
 
-    if (newText.value !== "") {
-        let newElement = document.createElement('li');
-        newElement.textContent = newText.value;
-        appendTo.appendChild(newElement);
-        newText.value = '';
+    if (outputField.value !== '') {
+        let newEl = document.createElement('li')
+        newEl.textContent = outputField.value
+        ulRef.appendChild(newEl)
+
+        outputField.value = ''
     }
 }
