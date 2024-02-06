935aaa65d761535201da8002fe8396467e2255ff
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-06 10:54:04 +0200

    Update report.js

--- JS-Advanced/domIntroductionExercise/08. Generate Report/report.js
+++ JS-Advanced/domIntroductionExercise/08. Generate Report/report.js
@@ -2,15 +2,31 @@ function generateReport() {
     let checkedHeaders = []
     let output = document.getElementById('output')
     let tableHeaders = document.getElementsByTagName('th');
+    let result = []
 
     for (let header of tableHeaders) {
-        let checkBox = header.querySelector('[type="checkbox"]')
-        if (checkBox.checked == true) {
-            inputElement = document.querySelector('input[name="employee"]')
-            checkedHeaders.push(inputElement.name)
-        }
+
+        let inputElement = header.querySelector("th input[type='checkbox']")
+        checkedHeaders.push(inputElement.name)
+
     }
 
-    let tableRows = document.querySelector('table').children
-    console.log(tableRows)
+    let tableRows = document.querySelectorAll('tbody tr');
+    let headers = document.querySelectorAll("th input[type='checkbox']")
+
+    for (let row of tableRows) {
+        let emplObject = {}
+        let tableData = row.querySelectorAll('td');
+
+        for (let i = 0; i < tableData.length; i++) {
+            let checkBox = headers[i]
+            if (checkBox.checked) {
+                emplObject[checkedHeaders[i]] = tableData[i].textContent
+            }
+        }
+
+        result.push(emplObject)
+    }
+
+    output.value = JSON.stringify(result)
 }
