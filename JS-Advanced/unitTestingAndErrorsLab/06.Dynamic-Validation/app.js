b79d11bc1c941ff1ca2c86a0f6c9cd3dfd908b9e
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-12 17:57:20 +0200

    Update app.js

--- JS-Advanced/unitTestingAndErrorsLab/06.Dynamic-Validation/app.js
+++ JS-Advanced/unitTestingAndErrorsLab/06.Dynamic-Validation/app.js
@@ -1,3 +1,18 @@
 function validate() {
-    // TODO
+    let inputRef = document.getElementById('email')
+    inputRef.addEventListener('change', onValidate)
+
+    function onValidate(e) {
+        let pattern = /[a-z]+\@[a-z]+\.[a-z]+/
+        let emailValue = inputRef.value
+        let match = emailValue.match(pattern)
+
+        if (!match) {
+            e.target.classList.add('error')
+        }
+
+        if (match) {
+            e.target.classList.remove('error')
+        }
+    }
 }
