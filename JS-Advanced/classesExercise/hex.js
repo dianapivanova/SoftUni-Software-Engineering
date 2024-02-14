d8a5f78d3234cb5686f83f39e7f72892c93497d0
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-14 12:32:09 +0200

    Update hex.js

--- JS-Advanced/classesExercise/hex.js
+++ JS-Advanced/classesExercise/hex.js
@@ -1,14 +1,37 @@
 class Hex {
-    constructor(value) {
-        this.value = value;
+
+    constructor(number) {
+        this.value = Number(number);
     }
 
     valueOf() {
-        return this.value
+        return this.value;
     }
 
     toString() {
-        return ('0x' + this.value.toString(16))
+        return `0x${(this.value.toString(16)).toUpperCase()}`;
     }
 
+    plus(number) {
+        let result = (this.value + number);
+        return new Hex(result);
+    }
+
+    minus(number) {
+        let result = (this.value - number);
+        return new Hex(result);
+    }
+
+    parse(text) {
+        return parseInt(text, 16);
+    }
 }
+
+let FF = new Hex(255);
+console.log(FF.toString());
+FF.valueOf() + 1 == 256;
+let a = new Hex(10);
+let b = new Hex(5);
+console.log(a.plus(b));
+console.log(a.plus(b).toString() === '0xF');
+console.log(FF.parse('AAA'));
