5cd14d24e96ea61f1b10304f91690cc78cc7fa8e
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-19 18:46:27 +0200

    Create wineSelection.js

---
+++ JS-Advanced/examPreparation/classProblems/wineSelection.js
@@ -0,0 +1,86 @@
+class WineSelection {
+    constructor(space) {
+        this.space = space;
+        this.wines = [];
+        this.bill = 0;
+    }
+
+    reserveABottle(wineName, wineType, price) {
+        if (this.space == this.wines.length) {
+            throw new Error(`Not enough space in the cellar.`);
+        }
+
+        this.wines.push({ wineName, wineType, price, paid: false });
+        return `You reserved a bottle of ${wineName} ${wineType} wine.`
+    }
+    payWineBottle(wineName, price) {
+        let findWine = this.wines.find(x => x.wineName == wineName);
+        if (!findWine) {
+            throw new Error(`${wineName} is not in the cellar.`)
+        }
+        if (findWine.paid == true) {
+            throw new Error(`${wineName} has already been paid.`)
+        } else {
+
+            findWine.paid = true;
+            this.bill += price;
+            return `You bought a ${wineName} for a ${price}$.`
+        }
+    }
+    openBottle(wineName) {
+        let findWine = this.wines.find(x => x.wineName == wineName);
+        if (!findWine) {
+            throw new Error(`The wine, you're looking for, is not found.`);
+        }
+        if (findWine.paid == false) {
+            throw new Error(`${wineName} need to be paid before open the bottle.`);
+        } else {
+            let idx = this.wines.indexOf(findWine);
+            this.wines.splice(idx, 1);
+            return `You drank a bottle of ${wineName}.`
+        }
+    }
+    cellarRevision(wineType) {
+        if (wineType == undefined) {
+            let sortedArr = this.wines.sort((a, b) => (a.wineName.localeCompare(b.wineName)));
+            let result = [];
+            result.push(`You have space for ${this.space - this.wines.length} bottles more.`);
+            result.push(`You paid ${this.bill}$ for the wine.`)
+
+            for (let wine of sortedArr) {
+                let hasPaid = '';
+                if (wine.paid == false) {
+                    hasPaid = 'Not Paid'
+                } else {
+                    hasPaid = 'Has Paid'
+                }
+                result.push(`${wine.wineName} > ${wine.wineType} - ${hasPaid}.`)
+            }
+
+            return result.join('\n');
+
+        } else {
+            let findWine = this.wines.find(x => x.wineType == wineType);
+            if (!findWine) {
+                throw new Error(`There is no ${wineType} in the cellar.`);
+            } else {
+                let hasPaid = '';
+                if (findWine.paid == false) {
+                    hasPaid = 'Not Paid'
+                } else {
+                    hasPaid = 'Has Paid'
+                }
+                return `${findWine.wineName} > ${findWine.wineType} - ${hasPaid}.`
+            }
+        }
+    }
+}
+const selection = new WineSelection(5)
+selection.reserveABottle('Bodegas Godelia Mencía', 'Rose', 144);
+selection.payWineBottle('Bodegas Godelia Mencía', 144);
+selection.reserveABottle('Sauvignon Blanc Marlborough', 'White', 50);
+selection.reserveABottle('Cabernet Sauvignon Napa Valley', 'Red', 120);
+console.log(selection.cellarRevision());
+
+
+
