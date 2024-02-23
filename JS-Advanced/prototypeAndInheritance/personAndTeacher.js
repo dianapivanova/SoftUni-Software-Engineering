2862c1564148bd94f240fffe0e89359252448c72
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-23 19:39:40 +0200

    HTTP exercise JSON files

--- JS-Advanced/prototypeAndInheritance/personAndTeacher.js
+++ JS-Advanced/prototypeAndInheritance/personAndTeacher.js
@@ -1,18 +1,17 @@
 function personAndTeacher() {
-
     class Person {
         constructor(name, email) {
             this.name = name;
             this.email = email;
-        }
-    }
+        };
 
+    }
     class Teacher extends Person {
-        constructor(name, email, subject) {
-            super(name, email);
+        constructor(subject) {
+            super(this.name, this.email);
             this.subject = subject;
         }
     }
 
-    return { Person, Teacher }
+    return { Person, Teacher };
 }
