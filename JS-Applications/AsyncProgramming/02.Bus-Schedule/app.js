7111790f6986c363458c556b2e2656f7e64070f9
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-02-28 23:49:36 +0200

    create app.js

--- JS-Applications/AsyncProgramming/02.Bus-Schedule/app.js
+++ JS-Applications/AsyncProgramming/02.Bus-Schedule/app.js
@@ -1,12 +1,44 @@
 function solve() {
 
-    function depart() {
-        console.log('Depart TODO...');
+    let nextStopId = 'depot';
+    const infoBox = document.querySelector('.info');
+    const departBtn = document.getElementById('depart');
+    const arriveBtn = document.getElementById('arrive');
+
+
+    let stopName = '';
+
+
+    async function depart() {
+        try {
+            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`);
+
+            if (!response.ok) {
+                const error = new Error(response);
+                throw error;
+            }
+
+            const data = await response.json();
+
+            stopName = data.name;
+            nextStopId = data.next;
+            infoBox.textContent = `Next stop ${stopName}`
+            departBtn.disabled = true;
+            arriveBtn.disabled = false;
+        } catch (error) {
+            infoBox.textContent = 'Error';
+            departBtn.disabled = true;
+            arriveBtn.disabled = true;
+        }
+
     }
 
     function arrive() {
-        console.log('Arrive TODO...');
+        departBtn.disabled = false;
+        arriveBtn.disabled = true;
+        infoBox.textContent = `Arriving at ${stopName}`
     }
+
 
     return {
         depart,
