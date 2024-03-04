7cde56412d65bbb28e9e4e099aeffee2b19ff4a5
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-03-04 14:55:30 +0200

    update app.js

--- JS-Applications/AsyncProgramming/02.Bus-Schedule/app.js
+++ JS-Applications/AsyncProgramming/02.Bus-Schedule/app.js
@@ -1,34 +1,35 @@
 function solve() {
 
-    let nextStopId = 'depot';
-    const infoBox = document.querySelector('.info');
     const departBtn = document.getElementById('depart');
+    departBtn.addEventListener('click', depart);
+
     const arriveBtn = document.getElementById('arrive');
+    arriveBtn.addEventListener('click', arrive);
 
+    const textArea = document.querySelector('.info');
 
+    let stopId = 'depot';
     let stopName = '';
 
+    async function depart(e) {
+        const url = `http://localhost:3030/jsonstore/bus/schedule/${stopId}`;
 
-    async function depart() {
         try {
-            const response = await fetch(`http://localhost:3030/jsonstore/bus/schedule/${nextStopId}`);
-
+            const response = await fetch(url);
             if (!response.ok) {
-                const error = new Error(response);
-                throw error;
+                const error = await response.json();
+                throw error
             }
-
             const data = await response.json();
-
             stopName = data.name;
-            nextStopId = data.next;
-            infoBox.textContent = `Next stop ${stopName}`
-            departBtn.disabled = true;
+            stopId = data.next;
+            textArea.textContent = `Next Stop: ${stopName}`
             arriveBtn.disabled = false;
-        } catch (error) {
-            infoBox.textContent = 'Error';
             departBtn.disabled = true;
+        } catch (err) {
             arriveBtn.disabled = true;
+            departBtn.disabled = true;
+            textArea.textContent = 'Error';
         }
 
     }
@@ -38,7 +39,6 @@ function solve() {
         arriveBtn.disabled = true;
         infoBox.textContent = `Arriving at ${stopName}`
     }
-
 
     return {
         depart,
