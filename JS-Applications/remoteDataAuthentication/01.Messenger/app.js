49c3764181a3549d180533a00512a1f7537b20d6
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-03-04 15:43:37 +0200

    create app.js

--- JS-Applications/remoteDataAuthentication/01.Messenger/app.js
+++ JS-Applications/remoteDataAuthentication/01.Messenger/app.js
@@ -1,57 +1,38 @@
 function attachEvents() {
-    const nameRef = document.querySelector('input[name="author"]');
-    const messageRef = document.querySelector('input[name="content"]');
-    const msgArea = document.getElementById('messages');
+    const textArea = document.getElementById('messages');
+    const nameInput = document.querySelector('input[name="author"]');
+    const messageInput = document.querySelector('input[name="content"]');
+    const url = `http://localhost:3030/jsonstore/messenger`;
+
     const sendBtn = document.getElementById('submit');
-    sendBtn.addEventListener('click', onSubmit);
-
     const refreshBtn = document.getElementById('refresh');
-    refreshBtn.addEventListener('click', onDisplay);
 
-    async function onSubmit(e) {
-        let author = nameRef.value;
-        let content = messageRef.value;
+    sendBtn.addEventListener('click', onSend);
+    refreshBtn.addEventListener('click', onRefresh); 4
 
-        try {
+    async function onSend() {
+        let author = nameInput.value;
+        let content = messageInput.value;
 
-            let url = `http://localhost:3030/jsonstore/messenger`;
-            const request = await fetch(url, {
-                method: 'post',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({ author, content })
-            })
+        let request = await fetch(url, {
+            method: 'post',
+            headers: { 'Content-type': 'application/json' },
+            body: JSON.stringify({ author, content })
+        });
 
-            if (!request.ok) {
-                const error = await request.json();
-                throw new Error(error.message);
-            }
-        } catch (err) {
-            alert(err.message);
-        }
     }
 
-    async function onDisplay() {
-        const url = `http://localhost:3030/jsonstore/messenger`;
+    async function onRefresh() {
+        let response = await fetch(url);
+        const data = await response.json();
 
-        try {
-            const response = await fetch(url);
-
-            if (!response.ok) {
-                const error = await response.json();
-                throw new Error(error);
-            };
-
-            const result = [];
-            const data = await response.json();
-            Object.entries(data).forEach(x => {
-                result.push(`${x[1].author}: ${x[1].content}`)
-            });
-            console.log(Object.entries(data))
-            msgArea.value = result.join('\n');
-        } catch (err) {
-            alert(err.message)
-        }
+        let result = [];
+        Object.values(data).forEach(x => {
+            result.push(`${x.author}: ${x.content}`);
+        })
+        textArea.value = result.join('\n');
     }
+
 }
 
 attachEvents();
