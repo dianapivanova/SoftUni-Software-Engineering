ad33c47c3698c6dbd5643b968efe10a451b608c6
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-04-01 13:19:30 +0300

    Update app.js

--- EXAM_PREP_DOM/gemCollection/app.js
+++ EXAM_PREP_DOM/gemCollection/app.js
@@ -1,14 +1,15 @@
 window.addEventListener("load", solve);
 
 function solve() {
-  const previewList = document.getElementById("preview-list");
-  const collection = document.getElementById("collection");
   const gemNameRef = document.getElementById("gem-name");
   const colorRef = document.getElementById("color");
   const caratsRef = document.getElementById("carats");
   const priceRef = document.getElementById("price");
-  const typeRef = document.getElementById("type");
+  const preciousRef = document.getElementById("type");
   const addBtn = document.getElementById("add-btn");
+  const previewList = document.getElementById("preview-list");
+  const collection = document.getElementById("collection");
+
   addBtn.addEventListener("click", onDefault);
 
   function onDefault(e) {
@@ -18,90 +19,89 @@ function solve() {
       colorRef.value == "" ||
       caratsRef.value == "" ||
       priceRef.value == "" ||
-      typeRef.value == ""
+      preciousRef.value == ""
     ) {
       return;
     }
 
     let gemName = gemNameRef.value;
     let color = colorRef.value;
-    let carats = caratsRef.value;
+    let carats = caratsRef.value ;
     let price = priceRef.value;
-    let type = typeRef.value;
+    let precious = preciousRef.value;
 
-    let liElement = document.createElement("li");
-    liElement.classList.add("gem-info");
-    let article = document.createElement("article");
-    let gemNamePar = document.createElement("h4");
-    gemNamePar.textContent = `${gemName}`;
-    let colorPar = document.createElement("p");
-    colorPar.textContent = `Color: ${color}`;
-    let caratsPar = document.createElement("p");
-    caratsPar.textContent = `Carats: ${carats}`;
-    let pricePar = document.createElement("p");
-    pricePar.textContent = `Price: ${price}$`;
-    let typePar = document.createElement("p");
-    typePar.textContent = `Type: ${type}`;
+    const liElement = document.createElement('li');
+    liElement.classList.add('gem-info');
+    const article = document.createElement('article');
+    const h4El = document.createElement('h4');
+    h4El.textContent = gemName;
+    const par1 = document.createElement('p');
+    par1.textContent = `Color: ${color}`;
+    const par2 = document.createElement('p');
+    par2.textContent = `Carats: ${carats}`;
+    const par3 = document.createElement('p');
+    par3.textContent = `Price: ${price}$`;
+    const par4 = document.createElement('p');
+    par4.textContent = `Type: ${precious}`;
 
-    article.appendChild(gemNamePar);
-    article.appendChild(colorPar);
-    article.appendChild(caratsPar);
-    article.appendChild(pricePar);
-    article.appendChild(typePar);
+    article.appendChild(h4El);
+    article.appendChild(par1);
+    article.appendChild(par2);
+    article.appendChild(par3);
+    article.appendChild(par4);
     liElement.appendChild(article);
 
-    let saveBtn = document.createElement("button");
-    saveBtn.classList.add("save-btn");
-    saveBtn.textContent = "Save to Collection";
+    const saveBtn = document.createElement('button');
+    saveBtn.textContent = `Save to Collection`;
+    saveBtn.classList.add('save-btn');
+    
+    const editBtn = document.createElement('button');
+    editBtn.textContent = `Edit information`;
+    editBtn.classList.add('edit-btn');
 
-    let editBtn = document.createElement("button");
-    editBtn.classList.add("edit-btn");
-    editBtn.textContent = "Edit Information";
-
-    let cancelBtn = document.createElement("button");
-    cancelBtn.classList.add("cancel-btn");
-    cancelBtn.textContent = "Cancel";
+    const cancelBtn = document.createElement('button');
+    cancelBtn.textContent = `Cancel`;
+    cancelBtn.classList.add('cancel-btn');
 
     liElement.appendChild(saveBtn);
     liElement.appendChild(editBtn);
     liElement.appendChild(cancelBtn);
-
     previewList.appendChild(liElement);
 
-    saveBtn.addEventListener("click", onSave);
-    editBtn.addEventListener("click", onEdit);
-    cancelBtn.addEventListener("click", onCancel);
+    saveBtn.addEventListener('click', onSave);
+    editBtn.addEventListener('click', onEdit);
+    cancelBtn.addEventListener('click', onCancel);
 
     addBtn.disabled = true;
-    gemNameRef.value = "";
-    colorRef.value = "";
-    caratsRef.value = "";
-    priceRef.value = "";
-    typeRef.value = "";
+    gemNameRef.value = ""
+    colorRef.value = ""
+    caratsRef.value = ""
+    priceRef.value = ""
+    preciousRef.value = ""
 
     function onSave(e) {
-        let newLiElement = document.createElement('li');
-        let newP = document.createElement('p');
-        newP.classList.add('collection-item');
-        newP.textContent = `${gemName} - Color: ${color}/ Carats: ${carats}/ Price: ${price}$/ Type: ${type}`;
-        newLiElement.appendChild(newP);
-        collection.appendChild(newLiElement);
-        liElement.remove();
-        addBtn.disabled = false;
+      liElement.remove();
+      let newLiElement = document.createElement('li');
+      let par = document.createElement('p');
+      par.classList.add('collection-item');4
+      par.textContent = `${gemName} - Color: ${color}/ Carats: ${carats}/ Price: ${price}$/ Type: ${precious}`;
+      newLiElement.appendChild(par);
+      collection.appendChild(newLiElement);
     }
+
     function onEdit(e) {
-        liElement.remove();
-        addBtn.disabled = false;
-        gemNameRef.value = gemName;
-        colorRef.value = color;
-        caratsRef.value = carats;
-        priceRef.value = price;
-        typeRef.value = type;
+      liElement.remove();
+      addBtn.disabled = false;
+      gemNameRef.value = gemName
+      colorRef.value = color
+      caratsRef.value = carats;
+      priceRef.value = price;
+      preciousRef.value = precious;
     }
 
     function onCancel(e) {
-        liElement.remove();
-        addBtn.disabled = false;
+      liElement.remove();
+      addBtn.disabled = false;
     }
   }
 }
