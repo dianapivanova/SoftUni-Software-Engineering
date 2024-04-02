311f56670e6e0a29b77c8ac3bc183b56e3e1740b
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-04-02 15:53:44 +0300

    Update findApartment.js

--- EXAM_PREP_unittesting/findApartment.js
+++ EXAM_PREP_unittesting/findApartment.js
@@ -1,105 +1,172 @@
 const findNewApartment = {
-    isGoodLocation(city, nearPublicTransportation) {
-      if (typeof city !== "string" || typeof nearPublicTransportation !== "boolean"){
-          throw new Error("Invalid input!");
-      }
-      if (city !== "Sofia" && city !== "Plovdiv" && city !== "Varna") {
-          return "This location is not suitable for you.";
-      }else {
-          if (nearPublicTransportation == true) {
-              return "You can go on home tour!";
-          }
-          else {
-              return "There is no public transport in area.";
-          }
-      }
-    },
-    isLargeEnough(apartments, minimalSquareMeters) {
-      let resultArr = [];
-      if (!Array.isArray(apartments) || typeof minimalSquareMeters !== "number" || apartments.length == 0) {
-        throw new Error("Invalid input!");
-      }
-      apartments.map((apartment) => {
-        if (apartment >= minimalSquareMeters) {
-          resultArr.push(apartment);
-        }
-      });
-      return resultArr.join(', ');
-    },
-    isItAffordable(price, budget) {
-      if (typeof price !== "number" || typeof budget !== "number"
-       || price <= 0 || budget <= 0) {
-        throw new Error("Invalid input!");
-      }
-      let result = budget - price;
-      if (result < 0) {
-        return "You don't have enough money for this house!";
+  isGoodLocation(city, nearPublicTransportation) {
+    if (
+      typeof city !== "string" ||
+      typeof nearPublicTransportation !== "boolean"
+    ) {
+      throw new Error("Invalid input!");
+    }
+    if (city !== "Sofia" && city !== "Plovdiv" && city !== "Varna") {
+      return "This location is not suitable for you.";
+    } else {
+      if (nearPublicTransportation == true) {
+        return "You can go on home tour!";
       } else {
-        return "You can afford this home!";
+        return "There is no public transport in area.";
       }
-    },
-  };
+    }
+  },
+  isLargeEnough(apartments, minimalSquareMeters) {
+    let resultArr = [];
+    if (
+      !Array.isArray(apartments) ||
+      typeof minimalSquareMeters !== "number" ||
+      apartments.length == 0
+    ) {
+      throw new Error("Invalid input!");
+    }
+    apartments.map((apartment) => {
+      if (apartment >= minimalSquareMeters) {
+        resultArr.push(apartment);
+      }
+    });
+    return resultArr.join(", ");
+  },
+  isItAffordable(price, budget) {
+    if (
+      typeof price !== "number" ||
+      typeof budget !== "number" ||
+      price <= 0 ||
+      budget <= 0
+    ) {
+      throw new Error("Invalid input!");
+    }
+    let result = budget - price;
+    if (result < 0) {
+      return "You don't have enough money for this house!";
+    } else {
+      return "You can afford this home!";
+    }
+  },
+};
 
-  import { expect } from "chai";
+import { expect } from "chai";
 
-  describe("Testing findNewApartment functionality", function() {
-    describe("testing •	isGoodLocation (city, nearPublicTransportation) ", function() {
+describe("testing findNewApartment functionality", function () {
+  describe("testing •isGoodLocation (city, nearPublicTransportation)", function () {
+    it("throws with invalid params", function () {
+      expect(() => findNewApartment.isGoodLocation({}, {})).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("throws with both params invalid", function() {
-            expect(() => findNewApartment.isGoodLocation(1, 1)).to.throw('Invalid input!')
-        });
+    it("throws with invalid params", function () {
+      expect(() => findNewApartment.isGoodLocation([], [])).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("returns not suitable with invalid location", function() {
-            expect(findNewApartment.isGoodLocation("test", false)).to.equal('This location is not suitable for you.')
-        });
+    it("throws with invalid params", function () {
+      expect(() => findNewApartment.isGoodLocation(false, "test")).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("returns no public transport with a false", function() {
-            expect(findNewApartment.isGoodLocation("Sofia", false)).to.equal('There is no public transport in area.')
-        });
+    it("returns with the city diff from S, P, V", function () {
+      expect(findNewApartment.isGoodLocation("test", true)).to.equal(
+        "This location is not suitable for you."
+      );
+    });
 
-        it("returns appropriately with valid city and true as second param", function() {
-            expect(findNewApartment.isGoodLocation("Sofia", true)).to.equal('You can go on home tour!')
-        });
-        
-     });
+    it("returns if the second param is false", function () {
+      expect(findNewApartment.isGoodLocation("Sofia", false)).to.equal(
+        "There is no public transport in area."
+      );
+    });
 
-     describe("testing •	isLargeEnough (apartments, minimalSquareMeters)", function() {
+    it("returns if voth params are valid", function () {
+      expect(findNewApartment.isGoodLocation("Sofia", true)).to.equal(
+        "You can go on home tour!"
+      );
+    });
+  });
 
-        it("throws with both params invalid", function() {
-            expect(() => findNewApartment.isLargeEnough(undefined, undefined)).to.throw('Invalid input!')
-        });
+  describe("testing •isLargeEnough (apartments, minimalSquareMeters)", function () {
+    
+    it("throws with an empty array", function () {
+      expect(() => findNewApartment.isLargeEnough([], 50)).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("throws with empty array", function() {
-            expect(() => findNewApartment.isLargeEnough([], 200)).to.throw('Invalid input!')
-        });
+    it("throws with second param not a num", function () {
+      expect(() => findNewApartment.isLargeEnough(['test', 'test', 'test'], 'test')).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("throws with empty array", function() {
-            expect(findNewApartment.isLargeEnough([20,20,20], 10)).to.deep.equal("20, 20, 20")
-        });
-     });
+    it("throws with first param not an array", function () {
+      expect(() => findNewApartment.isLargeEnough({}, 'test')).to.throw(
+        "Invalid input!"
+      );
+    });
 
-     describe("testing •	isItAffordable (price, budget)", function() {
+    it("throws with invalid params", function () {
+      expect(() => findNewApartment.isLargeEnough({}, {})).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("throws with both params invalid", function() {
-            expect(() => findNewApartment.isItAffordable(undefined, undefined)).to.throw('Invalid input!')
-        });
+    it("throws with invalid params", function () {
+      expect(() => findNewApartment.isLargeEnough(null, null)).to.throw(
+        "Invalid input!"
+      );
+    });
 
-        it("throws with both params less than 0", function() {
-            expect(() => findNewApartment.isItAffordable(-5, -5)).to.throw('Invalid input!')
-        });
+    it("works with valid params", function () {
+      expect(findNewApartment.isLargeEnough([40,50,60], 50)).to.deep.equal(
+        '50, 60'
+      );
+    });
 
-        it("throws with both params equal to 0", function() {
-            expect(() => findNewApartment.isItAffordable(0, 0)).to.throw('Invalid input!')
-        });
+    it("works with valid params", function () {
+      expect(findNewApartment.isLargeEnough([40,50,60], 40)).to.deep.equal(
+        '40, 50, 60'
+      );
+    });
 
-        it("throws with both params equal to 0", function() {
-            expect(findNewApartment.isItAffordable(500000, 200000)).to.equal("You don't have enough money for this house!")
-        });
+  });
 
-        it("throws with both params equal to 0", function() {
-            expect(findNewApartment.isItAffordable(200000, 500000)).to.equal("You can afford this home!")
-        });
-     });
+  describe("testing •isItAffordable (price, budget)", function () {
+    it("throws with first param not an array", function () {
+      expect(() => findNewApartment.isItAffordable({}, {})).to.throw(
+        "Invalid input!"
+      );
+    });
 
-     
+    it("throws with first param not an array", function () {
+      expect(() => findNewApartment.isItAffordable([], [])).to.throw(
+        "Invalid input!"
+      );
+    });
+
+    it("throws with first param not an array", function () {
+      expect(() => findNewApartment.isItAffordable('test', 'test')).to.throw(
+        "Invalid input!"
+      );
+    });
+
+    it("returns properly with valid params", function () {
+      expect(findNewApartment.isItAffordable(100000, 100000)).to.equal(
+        "You can afford this home!"
+      );
+    });
+
+    it("returns properly with valid params", function () {
+      expect(findNewApartment.isItAffordable(100000, 50000)).to.equal(
+        "You don't have enough money for this house!"
+      );
+    });
+  
+});
 });
