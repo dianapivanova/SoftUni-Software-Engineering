d1eda8c1cdae9bf4028a9d7afbb07dfdcf09307f
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-04-05 23:20:23 +0300

    Update app.js

--- JS-Applications/AsyncProgramming/04.Locked-Profile/app.js
+++ JS-Applications/AsyncProgramming/04.Locked-Profile/app.js
@@ -1,82 +1,66 @@
 async function lockedProfile() {
-    const main = document.getElementById('main');
-    main.innerHTML = '';
-
-    const url = 'http://localhost:3030/jsonstore/advanced/profiles';
-
-    const profilesData = await get(url);
-
-    Object.values(profilesData).forEach((profile, index) => {
-        const profileElement = createProfileCard(profile, index + 1);
-        main.appendChild(profileElement);
-    })
-
-    function createProfileCard(profile, count) {
-
-        const profileElement = document.createElement('div');
-        profileElement.className = 'profile';
-
-        profileElement.innerHTML = `
-				<img src="./iconProfile2.png" class="userIcon" />
-				<label>Lock</label>
-				<input type="radio" name="user${count}Locked" value="lock" checked>
-				<label>Unlock</label>
-				<input type="radio" name="user${count}Locked" value="unlock"><br>
-				<hr>
-				<label>Username</label>
-				<input type="text" name="user${count}Username" value="${profile.username}" disabled readonly />`;
-
-        const hiddenInfo = document.createElement('div');
-        hiddenInfo.id = `user${count}HiddenFields`;
-
-        hiddenInfo.innerHTML = `
-					<hr>
-					<label>Email:</label>
-					<input type="email" name="user${count}Email" value="${profile.email}" disabled readonly />
-					<label>Age:</label>
-					<input type="email" name="user${count}Age" value="${profile.age}" disabled readonly />`
-
-        hiddenInfo.style.display = 'none';
-        profileElement.appendChild(hiddenInfo);
-
-        const button = document.createElement('button');
-        button.textContent = 'Show more';
-        button.addEventListener('click', (e) => toggleInfo(e, hiddenInfo, count));
-
-        profileElement.appendChild(button);
-
-        return profileElement;
+  const url = `http://localhost:3030/jsonstore/advanced/profiles`;
+  let userNum = 1;
+  try {
+    let res = await fetch(url);
+    if (!res.ok) {
+      let err = await res.json();
+      throw new Error(err);
     }
+    let data = await res.json();
+    document.getElementById('main').innerHTML = '';
+    Object.values(data).forEach((x) => {
+      createUserCard(x, userNum);
+      userNum++;
+    });
+  } catch (err) {
+    alert(err.message);
+  }
 
-    function toggleInfo(e, hiddenInfo, count) {
-        const button = e.target;
-        const inputLock = document.querySelector(`[name="user${count}Locked"]`);
-
-        if (inputLock.checked == false) {
-            if (button.textContent == 'Show more') {
-                button.textContent = 'Hide it';
-                hiddenInfo.style.display = 'block';
-            } else if (button.textContent == 'Hide it') {
-                button.textContent = 'Show more';
-                hiddenInfo.style.display = 'none';
-            }
-        }
-    }
-
-
-    async function get(url) {
-        try {
-            const response = await fetch(url);
-            if (response.ok == false) {
-                const error = await response.json();
-                throw error;
-            }
-
-            const data = await response.json();
-            return data;
-
-        } catch (err) {
-            alert(`Error: ${err.message}`);
-        }
-    }
+  Array.from(document.querySelectorAll('.userHiddenFields')).forEach(x => x.style.display = 'none');
+  Array.from(document.querySelectorAll('main button')).forEach(x => x.addEventListener('click', onClick));
 }
+
+function createUserCard(obj, userNum) {
+  let divEl = document.createElement('div');
+  divEl.classList.add('profile');
+  divEl.innerHTML = `
+              <img src="./iconProfile2.png" class="userIcon" />
+              <label>Lock</label>
+              <input type="radio" name="user${userNum}Locked" value="lock" checked>
+              <label>Unlock</label>
+              <input type="radio" name="user${userNum}Locked" value="unlock"><br>
+              <hr>
+              <label>Username</label>
+              <input type="text" name="user${userNum}Username" value="${obj.username}" disabled readonly />
+              <div class="userHiddenFields">
+                  <hr>
+                  <label>Email:</label>
+                  <input type="email" name="user${userNum}Email" value="${obj.email}" disabled readonly />
+                  <label>Age:</label>
+                  <input type="text" name="user${userNum}Age" value="${obj.age}" disabled readonly />
+              </div>
+              
+              <button>Show more</button>
+          `;
+  
+        document.getElementById('main').appendChild(divEl);
+        
+}
+
+function onClick(e) {
+    if(e.target.textContent == 'Show more') {
+        if(e.target.parentElement.querySelector('input[type="radio"]').checked == false) {
+            e.target.parentElement.querySelector('.userHiddenFields').style.display = 'block';
+            e.target.textContent = 'Hide it';
+        }
+    } else if(e.target.textContent == 'Hide it') {
+        if(e.target.parentElement.querySelector('input[type="radio"]').checked == false) {
+            e.target.parentElement.querySelector('.userHiddenFields').style.display = 'none';
+            e.target.textContent = 'Show more';
+        }
+    }
+
+    
+}
+
