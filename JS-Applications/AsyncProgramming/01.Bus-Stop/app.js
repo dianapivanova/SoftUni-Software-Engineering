6e2c2b7b0b386c701069d60387144b7851619a61
Diana Ivanova <145497518+dianapivanova@users.noreply.github.com>
2024-04-05 17:04:57 +0300

    create Bus-Stop Async

--- JS-Applications/AsyncProgramming/01.Bus-Stop/app.js
+++ JS-Applications/AsyncProgramming/01.Bus-Stop/app.js
@@ -1,27 +1,29 @@
 async function getInfo() {
+  const inputRef = document.getElementById("stopId");
+  const submitBtn = document.getElementById("submit");
+  const resultRef = document.getElementById("stopName");
+  const busesRef = document.getElementById("buses");
+  inputRef.textContent = '';
+  busesRef.innerHTML = '';
 
-    const stopNameRef = document.getElementById('stopName');
-    const busesRef = document.getElementById('buses');
 
-    let stopId = document.getElementById('stopId').value;
-    let url = `http://localhost:3030/jsonstore/bus/businfo/${stopId}`;
+  let busStop = inputRef.value;
+  let url = `http://localhost:3030/jsonstore/bus/businfo/${busStop}`;
 
-    try {
-        let response = await fetch(url);
-        if (!response.ok) {
-            const error = new Error(response.statusText);
-            throw error;
-        }
-        let data = await response.json();
-        stopNameRef.textContent = data.name;
-        Object.entries(data.buses).forEach(x => {
-            let liEl = document.createElement('li');
-            liEl.textContent = `Bus ${x[0]} arrives in ${x[1]} minutes`;
-            busesRef.appendChild(liEl);
-        })
-
-    } catch (err) {
-        stopNameRef.textContent = 'Error';
+  try {
+    let response = await fetch(url);
+    if (!response.ok) {
+      throw new Error("Error");
     }
+    let data = await response.json();
+    resultRef.textContent = data.name;
+    let allBuses = Object.entries(data.buses);
+    allBuses.forEach(x => {
+        let liEl = document.createElement('li');
+        liEl.textContent = `Bus ${x[0]} arrives in ${x[1]} minutes`
+        busesRef.appendChild(liEl);
+    })
+  } catch (err) {
+    resultRef.textContent = "Error";
+  }
 }
-
